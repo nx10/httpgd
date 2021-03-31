b6745a4576c29bb1c3e072815b27046f62bd7e8f
Florian Rupprecht <floruppr@gmail.com>
2021-03-31 19:33:42 +0200

    Update readme and benchmark

--- README.md
+++ README.md
@@ -14,16 +14,19 @@ The device asynchronously serves SVG graphics via HTTP and WebSockets.
 
 ## Features
 
-* Fast high quality SVG plots
-* Stateless asynchronous HTTP/WebSocket API
-* Plot resizing
-* Plot history
-* HTML/JavaScript client (TypeScript module)
+* Fast SVG plots
+* Plot resizing and history
+* Interactive plot viewer (client)
+* Platform independent
 * Multiple concurrent clients
+* For developers:
+  * HTML/JavaScript client (TypeScript module)
+  * Stateless asynchronous HTTP/WebSocket API
+  
 
 ## Demo
 
-![demo](https://user-images.githubusercontent.com/33600480/83944385-6587fa80-a803-11ea-8f4a-7808d144309d.gif)
+![demo](https://user-images.githubusercontent.com/33600480/113182768-92eeda80-9253-11eb-9505-79de107024f7.gif)
 
 ## Installation
 
@@ -83,14 +86,15 @@ dev.off()
 | Keys | Result |
 |:----:|--------|
 | <kbd>&#8592;</kbd> <kbd>&#8594;</kbd> <kbd>&#8593;</kbd> <kbd>&#8595;</kbd> | Navigate plot history. |
-| <kbd>N</kbd> | Jump to the newest plot. |
-| <kbd>del</kbd> / <kbd>D</kbd> | Delete plot. |
 | <kbd>+</kbd> / <kbd>-</kbd> | Zoom in and out. |
 | <kbd>0</kbd> | Reset zoom level. |
+| <kbd>N</kbd> | Jump to the newest plot. |
+| <kbd>del</kbd> / <kbd>D</kbd> | Delete plot. |
+| <kbd>alt</kbd>+<kbd>D</kbd> | Clear all plots. |
 | <kbd>S</kbd> | Download plot as SVG. |
 | <kbd>P</kbd> | Download plot as PNG. |
 | <kbd>C</kbd> | Copy plot to clipboard (as PNG). |
-| <kbd>T</kbd> | Clear all plots. |
+| <kbd>H</kbd> | Toggle plot history (sidebar). |
 
 ### API &amp; Documentation
 
@@ -102,54 +106,11 @@ Technical documentation for developers wanting to contribute to `httpgd` can be 
 
 There are currently no other network graphics devices for comparison, `httpgd` can be used in offline mode (with `hgd(webserver = FALSE)`) to compare it with conventional SVG graphics devices.
 
-The [benchmark from svglite](https://github.com/r-lib/svglite/blob/master/README.md) has the following results:
+![](https://user-images.githubusercontent.com/33600480/113184973-232e1f00-9256-11eb-9595-327ec28ba360.png)
 
-<details>
+This benchmark compares `httpgd` 1.1.0 with `svglite` 2.0.0.
 
-<summary>Code</summary>
-
-```R
-library(svglite)
-library(httpgd)
-set.seed(1234)
-x <- runif(1e3)
-y <- runif(1e3)
-tmp1 <- tempfile()
-tmp2 <- tempfile()
-tmp3 <- tempfile()
-svglite_test <- function() {
-  svglite(tmp1)
-  plot(x, y)
-  dev.off()
-}
-svg_test <- function() {
-  svg(tmp2, onefile = TRUE)
-  plot(x, y)
-  dev.off()
-}
-httpgd_test <- function() {
-  hgd(webserver = FALSE)
-  plot(x, y)
-  hgd_svg(file = tmp3)
-  dev.off()
-}
-ben <-
-  bench::mark(httpgd_test(), svglite_test(), svg_test(), iterations = 250)
-```
-
-[See full code](https://github.com/nx10/httpgd/blob/master/docs/benchmark.R)
-
-</details>
-
-|expression     |    min| median|  itr/sec| mem_alloc|    gc/sec| n_itr| n_gc| total_time|
-|:--------------|------:|------:|--------:|---------:|---------:|-----:|----:|----------:|
-|httpgd_test()  | 10.2ms| 10.8ms| 91.43165|     361KB| 0.7373520|   248|    2|      2.71s|
-|svglite_test() | 20.3ms| 21.4ms| 46.29561|     593KB| 0.5622948|   247|    3|      5.33s|
-|svg_test()     | 27.2ms| 28.3ms| 35.15964|     126KB| 0.1412034|   249|    1|      7.08s|
-
-*Package versions: `httpgd` (1.0.0 dev), `svglite` (1.2.3.2), `grDevices` (4.0.3)*
-
-<img src="https://raw.githubusercontent.com/nx10/httpgd/master/docs/bench_speed1.png" width="640"/> <img src="https://raw.githubusercontent.com/nx10/httpgd/master/docs/bench_size1.png" width="640"/>
+[See benchmark code](https://github.com/nx10/httpgd/blob/master/docs/benchmark.R)
 
 
 ## System requirements
--- docs/benchmark.R
+++ docs/benchmark.R
@@ -1,81 +1,88 @@
 library(svglite)
 library(httpgd)
 
-set.seed(1234)
+# Benchmark: Time to plot
 
-x <- runif(1e3)
-y <- runif(1e3)
-tmp1 <- tempfile()
-tmp2 <- tempfile()
-tmp3 <- tempfile()
-
-svglite_test <- function() {
-  svglite(tmp1)
-  plot(x, y)
-  dev.off()
-}
-svg_test <- function() {
-  svg(tmp2, onefile = TRUE)
-  plot(x, y)
-  dev.off()
-}
-httpgd_test <- function() {
-  hgd(webserver = FALSE)
-  plot(x, y)
-  hgd_svg(file = tmp3)
-  dev.off()
-}
-
-ben <-
-  bench::mark(httpgd_test(), svglite_test(), svg_test(), iterations = 250)
-
-packages <- c("httpgd", "svglite", "grDevices")
-
-descrp <-
-  function(package_name) {
-    sprintf("%s (%s)",
-            package_name,
-            sapply(package_name, function(p)
-              paste(packageVersion(p))))
+results <- bench::press(
+  pts = 2^(0:10),
+  {
+    set.seed(1234)
+    x <- runif(pts)
+    y <- runif(pts)
+    
+    svglite_test <- function() {
+      stringSVG({
+        plot(x, y)
+      })
+    }
+    httpgd_test <- function() {
+      hgd_inline({
+        plot(x, y)
+      })
+    }
+    
+    bench::mark(httpgd_test(), svglite_test(), iterations = 128, check = FALSE)
   }
-pvers <- paste(descrp(packages), collapse = ", ")
+)
+
+# Benchmark: SVG size
+
+df <- data.frame(pts = 2^(0:12))
+
+df$pts
+df["svglite_test()"] <- vapply(df$pts, function(i) {
+  set.seed(1234)
+  x <- runif(i)
+  y <- runif(i)
+  nchar(stringSVG({
+    plot(x, y)
+  }))},
+  numeric(1)
+)
+df["httpgd_test()"] <- vapply(df$pts, function(i) {
+  set.seed(1234)
+  x <- runif(i)
+  y <- runif(i)
+  nchar(hgd_inline({
+    plot(x, y)
+  }))},
+  numeric(1)
+)
+
+# Merge data
+
+df <- tidyr::pivot_longer(df,c("svglite_test()", "httpgd_test()"), names_to = "expression", values_to = "chars")
+results$expression <- as.character(results$expression)
+df <- dplyr::inner_join(df, results)
+
+df$mem_alloc <- as.numeric(df$mem_alloc)
+dfmem <- tidyr::pivot_longer(df,c("mem_alloc", "chars"), names_to = "mem_type", values_to = "mem_val")
+
+# Plot results
 
 library(ggplot2)
 
-ggplot2::autoplot(ben) +
-  labs(title = "Plot speed (smaller is better)",
-       subtitle = pvers) +
-  scale_x_discrete(name  = "Package", labels = packages, limits = c("httpgd_test()", "svglite_test()", "svg_test()")) +
-  scale_y_continuous(name="Time", limits = c(0, NA)) +
-  theme_bw()
+g1 <- ggplot(df, aes(x=pts, y=as.numeric(median), colour=expression)) +
+  xlab('number of plot points') +
+  ylab('time to plot (sec)') +
+  scale_colour_discrete(name = '', labels=list(`svglite_test()`="svglite", `httpgd_test()`="httpgd")) +
+  geom_point() +
+  geom_line() +
+  theme_bw() + 
+  theme(legend.position="bottom")
 
-ggsave("docs/bench_speed1.png")
 
-df <- data.frame(name = c("svglite", "grDevices", "httpgd"),
-                 kb = file.size(c(tmp1, tmp2, tmp3)) / 1024)
+g2 <- ggplot(dfmem, aes(x=pts, y=mem_val/1024, colour=expression, shape=mem_type)) +
+  xlab('number of plot points') +
+  ylab('size (KB)') +
+  scale_shape_discrete(name = '', labels=list(chars="SVG size", mem_alloc="allocated memory")) +
+  scale_colour_discrete(name = '', labels=list(`svglite_test()`="svglite", `httpgd_test()`="httpgd")) +
+  geom_point() +
+  geom_line() +
+  theme_bw() + 
+  theme(legend.position="bottom", legend.box="vertical", legend.margin=margin())
 
-ggplot(data = df, aes(x = reorder(name, kb), y = kb)) +
-  geom_bar(stat = "identity") +
-  coord_flip() +
-  labs(
-    x = "Package",
-    y = "Size (kB)",
-    title = "SVG file size (smaller is better)",
-    subtitle = pvers
-  ) + theme_bw()
-ggsave("docs/bench_size1.png")
 
-cat(paste0(paste0(knitr::kable(ben[, c(
-  "expression",
-  "min",
-  "median",
-  "itr/sec",
-  "mem_alloc",
-  "gc/sec",
-  "n_itr",
-  "n_gc",
-  "total_time"
-)]), collapse = "\n"),
-"\n\n", pvers),
-file = "docs/bench_speed1.md",
-sep = "\n")
+
+gridExtra::grid.arrange(g1, g2, ncol = 2)
+
