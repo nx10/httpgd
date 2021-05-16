959b2de1272ce845bbb611c52890634600b7a7c4
Florian Rupprecht <floruppr@gmail.com>
2021-05-16 16:23:53 +0200

    Implement JSON renderer

---
+++ src/Base64.h
@@ -0,0 +1,12 @@
+#ifndef HTTPGD_BASE64_H
+#define HTTPGD_BASE64_H
+
+#include "DrawData.h"
+
+namespace httpgd
+{
+    std::string raster_base64(const dc::Raster &t_raster);
+
+} // namespace httpgd
+
+#endif // HTTPGD_BASE64_H
