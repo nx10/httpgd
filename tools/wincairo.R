57f2710d3476030b9d3b22c6cf7561196d05a99c
Florian Rupprecht <33600480+nx10@users.noreply.github.com>
2022-06-07 20:55:35 +0200

    Fix builds

--- tools/wincairo.R
+++
@@ -1,8 +0,0 @@
-VERSION <- commandArgs(TRUE)
-if(!file.exists(sprintf("../windows/cairo-%s/include/cairo/cairo.h", VERSION))){
-  if(getRversion() < "3.3.0") setInternet2()
-  download.file(sprintf("https://github.com/rwinlib/cairo/archive/v%s.zip", VERSION), "libcairo.zip", quiet = TRUE)
-  dir.create("../windows", showWarnings = FALSE)
-  unzip("libcairo.zip", exdir = "../windows")
-  unlink("libcairo.zip")
-}
