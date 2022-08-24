db0e53a5afcb3a44a6ff159e22226fe8efd3f87b
Florian Rupprecht <floruppr@gmail.com>
2022-08-24 17:05:51 +0200

    Various server tests for all endpoints

--- tests/testthat/test-server.R
+++ tests/testthat/test-server.R
@@ -1,3 +1,18 @@
+test_that("State status OK", {
+  hgd(token = FALSE, silent = TRUE)
+  res <- httr::GET(hgd_url("state"))
+  dev.off()
+  expect_equal(httr::status_code(res), 200)
+})
+
+test_that("live status OK", {
+  hgd(token = FALSE, silent = TRUE)
+  plot.new()
+  res <- httr::GET(hgd_url("live"))
+  dev.off()
+  expect_equal(httr::status_code(res), 200)
+})
+
 test_that("Render status OK", {
   hgd(token = FALSE, silent = TRUE)
   plot.new()
@@ -6,11 +21,21 @@ test_that("Render status OK", {
   expect_equal(httr::status_code(res), 200)
 })
 
+test_that("All renderers OK", {
+  hgd(token = FALSE, silent = TRUE)
+  plot.new()
+  vres <- vapply(unigd::ugd_renderers()$id, function(renderer_id) {
+    httr::status_code(httr::GET(hgd_url("plot", renderer=renderer_id))) == 200
+  }, logical(1))
+  dev.off()
+  expect_true(all(vres))
+})
+
 test_that("Plot identical", {
   hgd(token = FALSE, silent = TRUE)
   plot.new()
-  res <- httr::GET(hgd_url("plot", renderer = "svg"))
-  uplt <- unigd::ugd_render(as = "svg")
+  res <- httr::GET(hgd_url("plot", renderer = "json"))
+  uplt <- unigd::ugd_render(as = "json")
   dev.off()
   expect_equal(httr::text_content(res), uplt)
 })
@@ -36,4 +61,73 @@ test_that("Renderer info identical", {
   for (cname in colnames(df_res)) {
     expect_equal(df_res[[cname]], df_ugd[[cname]])
   }
+})
+
+test_that("Clear plots", {
+  fetch_json_hgd <- function(...) {
+    jsonlite::fromJSON(
+      httr::text_content(
+        httr::GET(
+          hgd_url(...))), simplifyVector = FALSE)
+  }
+
+  hgd(token = FALSE, silent = TRUE)
+  for (i in seq_len(10)) {
+    plot(0, main = sprintf("plot_%i", i))
+  }
+
+  json_plots <- fetch_json_hgd("plots")
+
+  expect_equal(length(json_plots$plots), 10)
+  expect_equal(json_plots$state$hsize, 10)
+
+  httr::GET(hgd_url("clear"))
+
+  json_plots <- fetch_json_hgd("plots")
+
+  expect_equal(length(json_plots$plots), 0)
+  expect_equal(json_plots$state$hsize, 0)
+
+  dev.off()
+})
+
+test_that("Delete plot", {
+  fetch_txt_hgd <- function(...) {
+      httr::text_content(
+        httr::GET(
+          hgd_url(...)))
+  }
+
+  hgd(token = FALSE, silent = TRUE)
+  for (i in seq_len(10)) {
+    plot(0, main = sprintf("plot_%i", i))
+  }
+
+  json_p4 <- fetch_txt_hgd(4, renderer = "json")
+  json_p5 <- fetch_txt_hgd(5, renderer = "json")
+
+  expect_true(grepl("\"str\": \"plot_4\"", json_p4))
+  expect_true(grepl("\"str\": \"plot_5\"", json_p5))
+
+  httr::GET(paste0(hgd_url("remove"), "?index=4"))
+
+  json_p4 <- fetch_txt_hgd(4, renderer = "json")
+  json_p5 <- fetch_txt_hgd(5, renderer = "json")
+
+  expect_true(grepl("\"str\": \"plot_4\"", json_p4))
+  expect_true(grepl("\"str\": \"plot_6\"", json_p5))
+
+  dev.off()
+})
+
+test_that("Delete plot status", {
+  hgd(token = FALSE, silent = TRUE)
+  for (i in seq_len(10)) {
+    plot(0, main = sprintf("plot_%i", i))
+  }
+
+  expect_equal(httr::status_code(httr::GET(paste0(hgd_url("remove"), "?index=4"))), 200)
+  expect_equal(httr::status_code(httr::GET(paste0(hgd_url("remove"), "?index=999"))), 404)
+
+  dev.off()
 })
